3fa90470d21a7f44ed4676fc3690a61baead761e
mgurkankaya <https://github.com/mgurkankaya>
2024-12-22 16:38:45 +0300

    Add project files.

---
+++ RapidApiProject/Controllers/ImdbMovieController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+
+namespace RapidApiProject.Controllers
+{
+    public class ImdbMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "af0486c0dfmsh9b9d3588c9bdd10p16fd14jsn539f91b4569a" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ImdbMovieViewModel>>(body);
+                return View(value.ToList());
+            }
+        }
+    }
+}
---
+++ RapidApiProject/Models/ImdbMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RapidApiProject.Models
+{
+    public class ImdbMovieViewModel
+    {
+        public string title { get; set; }
+        public string rating { get; set; }
+        public string imdb_link { get; set; }
+        public string big_image { get; set; }
+
+    }
+}
